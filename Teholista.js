2b7431c7eec9da4a06cdb18f3715050850a98d94
KHnakki <94570029+KHnakki@users.noreply.github.com>
2025-05-11 16:14:50 +0300

    Update Teholista.js

--- Teholista.js
+++ Teholista.js
@@ -1,16 +1,16 @@
 const users = {
-  admin: { username: 'admin', password: 'admin123' },
-  user: { username: 'user', password: 'user123' }
-};
-
-function login(role) {
-  const username = document.getElementById('username').value.trim();
-  const password = document.getElementById('password').value.trim();
-
-  if (username === users[role].username && password === users[role].password) {
-    localStorage.setItem('loggedUser', role);
-    window.location.href = role === 'admin' ? 'admin.html' : 'user.html';
-  } else {
-    alert('Virheellinen käyttäjätunnus tai salasana!');
+    admin: { username: 'admin', password: 'admin123' },
+    user: { username: 'user', password: 'user123' }
+  };
+  
+  function login(role) {
+    const username = document.getElementById('username').value.trim();
+    const password = document.getElementById('password').value.trim();
+  
+    if (username === users[role].username && password === users[role].password) {
+      localStorage.setItem('loggedUser', role);
+      window.location.href = role === 'admin' ? 'admin.html' : 'user.html';
+    } else {
+      alert('Virheellinen käyttäjätunnus tai salasana!');
+    }
   }
-}
